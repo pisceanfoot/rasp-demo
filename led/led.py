a5551b971d0c37a47a57f9328873b2846df12fbe
Leo <pisceanfoot@gmail.com>
2016-01-31 21:47:39 +0800

    add output

--- led/led.py
+++ led/led.py
@@ -6,11 +6,13 @@ PIN_NO = 12
 GPIO.setmode(GPIO.BOARD)
 GPIO.setup(PIN_NO, GPIO.OUT)
 
-for i in range(5):
-	print 'times %s' % (i + 1)
-	GPIO.output(PIN_NO,GPIO.HIGH)
-	time.sleep(2)
-	GPIO.output(PIN_NO,GPIO.LOW)
-	time.sleep(2)
-
-GPIO.cleanup()
+try:
+	for i in range(5):
+		print 'times %s' % (i + 1)
+		GPIO.output(PIN_NO,GPIO.HIGH)
+		time.sleep(2)
+		GPIO.output(PIN_NO,GPIO.LOW)
+		time.sleep(2)
+		GPIO.cleanup()
+except KeyboardInterrupt:
+	GPIO.cleanup()
