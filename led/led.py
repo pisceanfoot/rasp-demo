e689c08043d57409eb3d0d1136456cc7343886dd
Leo <pisceanfoot@gmail.com>
2016-01-31 21:12:37 +0800

    fix spell error

--- led/led.py
+++ led/led.py
@@ -8,8 +8,8 @@ GPIO.setup(PIN_NO, GPIO.OUT)
 
 for i in range(20):
 	GPIO.output(PIN_NO,GPIO.HIGH)
-	time.sleep(20)
+	time.sleep(2)
 	GPIO.output(PIN_NO,GPIO.LOW)
-	time.sleep(20)
+	time.sleep(2)
 
 GPIO.cleanup()
